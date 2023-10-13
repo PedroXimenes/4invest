23a80ac3ede289f2deffefd2bed0903c926fa9bb
Pedro Ximenes <pedro.ximenes@hatchworks.com>
2023-10-13 08:59:49 -0300

    adiciona cors

--- internal/pkg/api/router/router.go
+++ internal/pkg/api/router/router.go
@@ -3,9 +3,12 @@ package router
 import (
 	"github.com/PedroXimenes/4invest/internal/app/handlers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Router(app *fiber.App) {
+	app.Use(cors.New())
+
 	app.Get("/users", handlers.GetAll)
 	app.Get("/users/:id", handlers.Get)
 	app.Post("/users", handlers.Create)
