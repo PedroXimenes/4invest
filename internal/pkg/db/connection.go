d82d37c38561af394fb7baf10a99e7270bf21508
Pedro Ximenes <pedro.ximenes@hatchworks.com>
2023-10-13 09:00:21 -0300

    corrige problema de conexão com o postgres

--- internal/pkg/db/connection.go
+++ internal/pkg/db/connection.go
@@ -32,17 +32,20 @@ package db
 import (
 	"database/sql"
 
+	log "github.com/sirupsen/logrus"
+
 	"cloud.google.com/go/cloudsqlconn"
 	"cloud.google.com/go/cloudsqlconn/postgres/pgxv4"
 )
 
-func OpenConnection() (*sql.DB, error) {
+func init() {
 	cleanup, err := pgxv4.RegisterDriver("cloudsql-postgres", cloudsqlconn.WithIAMAuthN())
-	if err != nil {
-		return nil, err
-	}
 	defer cleanup()
-
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func OpenConnection() (*sql.DB, error) {
 	db, err := sql.Open(
 		"cloudsql-postgres",
 		"host=sincere-almanac-401411:us-central1:four-invest user=postgres password=m-,b(<(vIgsuXCV< dbname=four-invest sslmode=disable",
